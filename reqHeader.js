4f9d1294f37bd29a4cef090a1936e75b72958a3f
arialblack14 <tsirasalexis@yahoo.gr>
2016-03-28 15:59:57 +0300

    fix headers json

---
+++ reqHeader.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+    bodyParser = require('body-parser');
+
+
+exports.browser_type = function(ans) {
+  var browser_type = "";
+  // Check most common browsers from 'user-agent'
+  if (ans["user-agent"].match(/chrome/i)) {
+    browser_type = 'Chrome';
+  } else if (ans["user-agent"].match(/explorer/i)) {
+    browser_type = 'Internet Explorer';
+  } else if (ans["user-agent"].match(/safari/i) || ans["user-agent"].match(/iPhone/i)) {
+    browser_type = 'Safari';
+  } else if (ans["user-agent"].match(/firefox/i)) {
+    browser_type = 'Firefox';
+  } else if (ans["user-agent"].match(/opera/i)) {
+    browser_type = 'Opera';
+  } else {
+    browser_type = 'Unknown';
+  }
+  return browser_type;
+};
+
+exports.language = function(ans) {
+  return ans["accept-language"].split(";")[0]; // Get first language accepted
+};
+
+exports.os = function(ans) {
+  var os = "";
+  // Check most common browsers from 'user-agent'
+  if (ans["user-agent"].match(/linux/i)) {
+    os = 'Linux';
+  } else if (ans["user-agent"].match(/windows/i)) {
+    os = 'Windows';
+  } else if (ans["user-agent"].match(/apple/i) || ans["user-agent"].match(/iPhone/i)) {
+    os = 'Apple';
+  } else {
+    os = 'Unknown';
+  }
+  return os;
+};
--- routes/whoami.js
+++ routes/whoami.js
@@ -1,6 +1,9 @@
 var express = require('express'),
     bodyParser = require('body-parser');
 
+// Require the module which checks the headers
+var reqHeader = require('../reqHeader');
+
 var whoamiRouter = express.Router();
 whoamiRouter.use(bodyParser.json());
 
@@ -8,10 +11,11 @@ whoamiRouter.route('/')
 
 .get(function(req, res, next) {
   console.log(req.headers);
+
   var credentials = {
-    "OS": req.headers["user-agent"].split(' ')[8], // Get software from user-agent
-    "Browser": req.headers["user-agent"].split(' ')[9], // Get browser
-    "Language": req.headers["accept-language"].split(";")[0], // Get first language accepted
+    "OS": reqHeader.os(req.headers),
+    "Browser": reqHeader.browser_type(req.headers), // Get browser
+    "Language": reqHeader.language(req.headers),
     "IP": req.ip
   };
   res.send(credentials);
